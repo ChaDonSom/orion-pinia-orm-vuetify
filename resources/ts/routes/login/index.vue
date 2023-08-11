2149bba58071649aa62a5a720fca0e6cf2776cc7
Chase Drift Ubuntu <chadonsom@gmail.com>
2023-08-11 09:00:29 -0400

    Login card scaffold

--- resources/ts/routes/login/index.vue
+++ resources/ts/routes/login/index.vue
@@ -1,3 +1,9 @@
 <template>
-Login
+  <div class="flex h-screen">
+    <VCard title="Log in" class="m-auto">
+      <VCardActions>
+        <VBtn color="primary" class="text-none" rounded>Log in</VBtn>
+      </VCardActions>
+    </VCard>
+  </div>
 </template>
